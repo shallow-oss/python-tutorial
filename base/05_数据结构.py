a9d0a6c9e49906e242604a139a934e2c55c8cac5
ZhengzhanPeng <3136149356@qq.com>
2023-07-17 21:59:03 +0800

    2023-7-17(2)

--- base/05_数据结构.py
+++ base/05_数据结构.py
@@ -1,8 +1,9 @@
 # 列表详解
 # 初始化列表
+from collections import deque
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 
-# list.count(x) 
+# list.count(x)
 # 返回列表中元素 x 出现的次数
 # print()
 print(fruits.count('apple'))
@@ -43,7 +44,7 @@ print(fruits)
 
 # list.insert(i, x)
 # 在指定位置插入元素,第一个参数是插入元素的索引
-fruits.insert(2,'appcl')
+fruits.insert(2, 'appcl')
 print(fruits)
 
 # list.remove(x)
@@ -73,17 +74,16 @@ stack.pop()
 print(stack)
 
 # 用列表实现队列
-from collections import deque
 queue = deque(["Eric", "John", "Michael"])
-queue.append("Terry")     
-queue.append("Graham") 
-print(queue)        
-queue.popleft()                
-queue.popleft()                 
+queue.append("Terry")
+queue.append("Graham")
+print(queue)
+queue.popleft()
+queue.popleft()
 print(queue)
 
 # 列表推导式
-# 对序列或可迭代对象中的每个元素应用某种操作，用生成的结果创建新的列表；或用满足特定条件的元素创建子序列    
+# 对序列或可迭代对象中的每个元素应用某种操作，用生成的结果创建新的列表；或用满足特定条件的元素创建子序列
 # 无副作用地计算平方列表
 # squares = list(map(lambda x: x**2, range(10)))
 squares = [x**2 for x in range(10)]
@@ -93,5 +93,95 @@ print(squares)
 # 一个表达式  一个 for 子句 零个或多个 for 或 if 子句
 # 结果是由表达式依据 for 和 if 子句求值计算而得出一个新列表
 
+# 嵌套的列表推导式
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+new_matrix = [[row[i] for row in matrix] for i in range(4)]
+print(new_matrix)
+print(list(zip(*matrix)))
 
-# 5.1.3
+# del 语句
+# 按索引从列表中移除元素，而不是值
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+del a[0]
+print(a)
+# del 从列表中移除切片
+del a[2:4]
+print(a)
+# del 清空整个列表
+del a[:]
+print(a)
+
+# 元组和序列
+t = 12345, 54321, 'hello!'
+print(t[0])
+print(t)
+# 元组可以由元组组成
+u = t, (1, 2, 3, 4, 5)
+print(u)
+# 元组是不可变的
+# t[0] = 88888
+# 元组可以包含可变的数据类型
+v = ([1, 2, 3], [3, 2, 1])
+print(v)
+# 创建空元组
+empty = ()
+# 创建有一个元素的元组，需要在元素后加逗号
+singleton = 'hello',
+print(len(empty))
+print(len(singleton))
+print(singleton)
+
+# 集合
+# 集合是由不重复元素组成的无序容器
+# 初始化集合
+# 创建空集合只能使用set()
+empty = set()
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+print('orange' in basket)
+print('crabgrass' in basket)
+
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+print(b)
+# a 有 b 没有
+print(a - b)
+# a 有 b 也有
+print(a | b)
+# a b 都有的
+print(a & b)
+# a b 不共有的
+print(a ^ b)
+
+# 字典
+# 字典以 关键字 为索引，关键字通常是字符串或数字，也可以是其他任意不可变类型
+# 字典的键必须是唯一的
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+print(tel['jack'])
+del tel['sape']
+tel['irv'] = 4127
+print(tel)
+# list(字典) 返回该字典中所有键的列表
+print(list(tel))
+# 排列
+sorted(tel)
+print(tel)
+# 检查字典里是否存在某个键,使用 in
+print('guido' in tel)
+print('jack' not in tel)
+# dict() 构造函数可以直接用键值对序列创建字典
+dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+# 字典推导式可以用任意键值表达式创建字典
+{x: x**2 for x in (2, 4, 6)}
+# 关键字是比较简单的字符串时，直接用关键字参数指定键值对更便捷
+dict(sape=4139, guido=4127, jack=4098)
+
+
+# 5.6
