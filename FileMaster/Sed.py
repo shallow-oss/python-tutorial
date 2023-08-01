eb9fd98e87dd2a7e64a1ca03a87457f6d12e0fff
ZhengzhanPeng <3136149356@qq.com>
2023-08-01 21:54:28 +0800

    2023-8-1

--- FileMaster/Sed.py
+++ FileMaster/Sed.py
@@ -1,28 +1,46 @@
-def sed(command, file_mame):
+import re
+
+
+def getContent(file_name):
     try:
-        command = command.split('/')
-        match command[0]:
-            case 's':
-                print('替换匹配的字符串')
-                pass
-            case 'd':
-                print('删除匹配的行')
-                pass
-            case 'i':
-                print('匹配行前追加文本')
-                pass
-            case 'a':
-                print('匹配行后追加文本')
-                pass
-            case _:
-                print('输入错误')
-                pass
+        with open(file_name, 'r') as file:
+            content = file.read()
+        return content
     except FileNotFoundError:
         print("File is not Found.")
 
 
-sed('s/old/new', 'file.txt')
-sed('d/pattern', 'file.txt')
-sed('i/pattern/inserted text', 'file.txt')
-sed('a/pattern/inserted text', 'file.txt')
-sed('ad/pattern/inserted text', 'file.txt')
+def saveContent(file_name, content):
+    try:
+        with open(file_name, 'w') as file:
+            file.write(content)
+    except FileNotFoundError:
+        print("File is not Found.")
+
+
+def sed(command, file_name):
+    command = command.split('/')
+    match command[0]:
+        case 's' | 'd':
+            content = getContent(file_name)
+            pattern = command[1].encode("unicode_escape").decode()
+            replacement = command[2].encode(
+                "unicode_escape").decode() if command[0] == 's' else ''
+            new_content = re.sub(pattern, replacement, content)
+            saveContent(file_name, new_content)
+        case 'i':
+            print('匹配行前追加文本')
+            pass
+        case 'a':
+            print('匹配行后追加文本')
+            pass
+        case _:
+            print('输入错误')
+            pass
+
+
+sed('s/[0-9]/NUM', 'FileMaster\File.txt')
+sed('d/NUM', 'FileMaster\File.txt')
+sed('i/pattern/inserted text', 'FileMaster\File.txt')
+sed('a/pattern/inserted text', 'FileMaster\File.txt')
+sed('ad/pattern/inserted text', 'FileMaster\File.txt')
